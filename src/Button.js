bb919ddf63221d7d5250e2c4246bb97ea04c0af2
HiteshDatt <hdk.hitesh26@gmail.com>
2023-01-18 15:32:57 +0530

    created a test react ui library

---
+++ src/Button.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+require("./Button.css");
+function default_1({ title }) {
+    return (react_1.default.createElement("button", { className: "UI-Button" }, title));
+}
+exports.default = default_1;
