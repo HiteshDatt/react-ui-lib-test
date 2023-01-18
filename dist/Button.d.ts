1c701dc73b2b4cd3228dc7f33090e6b19d18efaa
HiteshDatt <hdk.hitesh26@gmail.com>
2023-01-18 16:46:31 +0530

    used tsc

---
+++ dist/Button.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import './Button.css';
+export default function ({ title }: {
+    title: string;
+}): JSX.Element;
---
+++ dist/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Button.css';
+export default function ({ title }) {
+    return (React.createElement("button", { className: "UI-Button-2" }, title));
+}
+//# sourceMappingURL=Button.js.map
